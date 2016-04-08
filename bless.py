981e7be9f9bd63aae4c5ac1928db9d5fe96573f0
Zhuoran Zhang <zz325@cl.cam.ac.uk>
2016-04-08 17:17:19 +0100

    Move computation of eigenvectors to bless script

--- bless.py
+++ bless.py
@@ -1,18 +1,26 @@
 import argparse
 import cPickle as pickle
+import time
 from dmatrices import DMatrices
 
-def process_bless(path, matrices, wordmap):
+def process_bless(path, matrices, wordmap, num_cores):
     with open(path, 'r') as f:
         bless = pickle.load(f)
     dm = DMatrices(matrices, wordmap)
-    rxy = dm.repres(bless)
+    words = set()
+    for a,b in bless:
+        words.add(a)
+        words.add(b)
+    t = time.time()
+    dm.get_eigenvectors(words, num_cores=num_cores)
+    print "Eigenvectors computed in %d seconds" % (time.time() - t)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate on BLESS.")
     parser.add_argument('path', type=str, help='path to preprocessed BLESS data')
     parser.add_argument('matrices', type=str, help='path to pickled matrices')
     parser.add_argument('wordmap', type=str, help='path to pickled wordmap')
+    parser.add_argument('--num_cores', type=int, help='number of cores to use', default=1)
     args = parser.parse_args()
-    process_bless(args.path, args.matrices, args.wordmap)
+    process_bless(args.path, args.matrices, args.wordmap, args.num_cores)
 
