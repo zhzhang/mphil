4795f7cf1a4cec25491ccef47ab0266722632d5d
Jordan Zhang <jordan.of.zhang@gmail.com>
2016-05-05 09:51:04 +0100

    Add subsampling to bless preprocess

--- read_bless.py
+++ read_bless.py
@@ -13,15 +13,19 @@ def parse_line(line):
     relu = relu.split('-')[0]
     return (con, relu, cla, rel)
 
-def process_bless(path, output_path):
+def process_bless(path, output_path, subsample=1):
     output = {}
+    count = 0
     with open(path, 'r') as f:
         for line in f:
             con, relu, cla, rel = parse_line(line)
             if rel == 'hyper':
                 output[(con, relu)] = (rel, cla)
             elif rel == 'random-n':
-                output[(con, relu)] = (rel, cla)
+                count += 1
+                if count == subsample:
+                    output[(con, relu)] = (rel, cla)
+                    count = 0
     if output_path:
         print "Saving output..."
         with open(output_path + '.txt', 'w+') as f:
@@ -36,5 +40,5 @@ if __name__ == "__main__":
     parser.add_argument('path', type=str, help='path to BLESS data set')
     parser.add_argument('--output', type=str, help='output file')
     args = parser.parse_args()
-    process_bless(args.path, args.output)
+    process_bless(args.path, args.output, subsample=4)
 
