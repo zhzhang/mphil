8ad280fcbf19f456a3b95139f05ffb03de060da7
Jordan Zhang <jordan.of.zhang@gmail.com>
2016-06-03 22:44:57 +0100

    update bless reading script

--- read_bless.py
+++ read_bless.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import cPickle 
+import copy
+import cPickle
+import math
 import os
+import random
 from preprocess import *
 
 def parse_line(line):
@@ -13,32 +16,61 @@ def parse_line(line):
     relu = relu.split('-')[0]
     return (con, relu, cla, rel)
 
-def process_bless(path, output_path, subsample=1):
-    output = {}
-    count = 0
-    with open(path, 'r') as f:
-        for line in f:
-            con, relu, cla, rel = parse_line(line)
-            if rel == 'hyper' or rel == 'coord' or rel == 'mero':
-                output[(con, relu)] = (rel, cla)
-            elif rel == 'random-n':
-                count += 1
-                if count == subsample:
-                    output[(con, relu)] = (rel, cla)
-                    count = 0
-    if output_path:
-        print "Saving output..."
-        with open(output_path + '.txt', 'w+') as f:
-            for pair in output:
-                f.write("%s %s\n" % pair)
-        with open(output_path + '.pkl', 'w+') as f:
-            pickle.dump(output, f)
+def write_data(name, data):
+    with open("targets/" + name + '.txt', 'w') as f:
+        for con, relu, _, _ in data:
+            f.write("%s %s\n" % (con, relu))
+    with open("targets/" + name + '.pkl', 'w') as f:
+        pickle.dump(data, f)
+
+def subsample(data):
+    concepts = set()
+    for con, relu, rel, cla in data:
+        concepts.add(con)
+    sample = random.sample(concepts, math.floor(concepts / 6))
+    output = []
+    for con, relu, rel, cla in data:
+        if con in sample:
+            concepts.add(con)
+    print "Random samples: %d" % len(output)
     return output
 
+def shuffle(data):
+    datacopy = copy.deepcopy(data)
+    random.shuffle(datacopy)
+    output = []
+    classmap = {}
+    for con, relu, cla, rel in data:
+        classmap[con] = cla
+    for con, _, cla, _ in data:
+        for point in datacopy:
+            if not classmap[point[0]] == cla:
+                output.append((con, point[1], cla, 'random-n'))
+                datacopy.remove(point)
+    return output
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Read in BLESS data set.")
-    parser.add_argument('path', type=str, help='path to BLESS data set')
-    parser.add_argument('--output', type=str, help='output file')
-    args = parser.parse_args()
-    process_bless(args.path, args.output, subsample=4)
+    if not os.path.exists("targets")
+        os.mkdir("targets")
+    nouns = []
+    with open("bless.txt", 'r') as f:
+        for line in f:
+            con, relu, cla, rel = parse_line(line)
+            if rel == 'hyper' or rel == 'coord' or rel == 'mero' or rel == 'random-n':
+                nouns.append((con, relu, rel, cla))
+    write_data("nouns", nouns)
+
+    hyper = []
+    random = []
+    with open("bless_random_test.txt", 'r') as f:
+        for line in f:
+            con, relu, cla, rel = parse_line(line)
+            if rel == 'hyper':
+                hyper.append((con, relu, rel, cla))
+            elif rel == 'random-n':
+                random.append((con, relu, rel, cla))
+    write_data("rand-hyper", hyper + subsample(random))
+    write_data("hyper", hyper) 
+    write_data("shuffle-hyper", hyper + shuffle(hyper)) 
 
