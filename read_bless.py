c3415bbb0e03bc5d7ff4f187df1a2ca7eb15d4d9
Zhuoran Zhang <zz325@cl.cam.ac.uk>
2016-06-05 21:22:07 +0100

    Updated read bless script

--- read_bless.py
+++ read_bless.py
@@ -3,11 +3,14 @@
 
 import argparse
 import copy
-import cPickle
+import cPickle as pickle
 import math
 import os
 import random
-from preprocess import *
+
+plant = ['tree', 'fruit', 'vegetable']
+nonliving = ['building', 'weapon', 'container', 'musical_instrument', 'appliance', 'tool', 'vehicle', 'clothing', 'furniture']
+animal = ['insect', 'water_animal', 'amphibian_reptile', 'bird', 'ground_mammal']
 
 def parse_line(line):
     # Concept, class, relation, relatum
@@ -25,13 +28,13 @@ def write_data(name, data):
 
 def subsample(data):
     concepts = set()
-    for con, relu, rel, cla in data:
+    for con, relu, cla, rel in data:
         concepts.add(con)
-    sample = random.sample(concepts, math.floor(concepts / 6))
+    sample = random.sample(concepts, int(math.floor(len(concepts) / 4.7)))
     output = []
-    for con, relu, rel, cla in data:
+    for con, relu, cla, rel in data:
         if con in sample:
-            concepts.add(con)
+            output.append((con, relu, cla, rel))
     print "Random samples: %d" % len(output)
     return output
 
@@ -44,33 +47,40 @@ def shuffle(data):
         classmap[con] = cla
     for con, _, cla, _ in data:
         for point in datacopy:
-            if not classmap[point[0]] == cla:
+            if not get_group(classmap[point[0]]) == get_group(cla):
                 output.append((con, point[1], cla, 'random-n'))
                 datacopy.remove(point)
     return output
 
+def get_group(cla):
+    if cla in nonliving:
+        return "nonliving"
+    elif cla in animal:
+        return "animal"
+    elif cla in plant:
+        return "plant"
 
 if __name__ == "__main__":
-    if not os.path.exists("targets")
+    if not os.path.exists("targets"):
         os.mkdir("targets")
     nouns = []
     with open("bless.txt", 'r') as f:
         for line in f:
             con, relu, cla, rel = parse_line(line)
             if rel == 'hyper' or rel == 'coord' or rel == 'mero' or rel == 'random-n':
-                nouns.append((con, relu, rel, cla))
+                nouns.append((con, relu, cla, rel))
     write_data("nouns", nouns)
 
     hyper = []
-    random = []
+    randomn = []
     with open("bless_random_test.txt", 'r') as f:
         for line in f:
             con, relu, cla, rel = parse_line(line)
             if rel == 'hyper':
-                hyper.append((con, relu, rel, cla))
+                hyper.append((con, relu, cla, rel))
             elif rel == 'random-n':
-                random.append((con, relu, rel, cla))
-    write_data("rand-hyper", hyper + subsample(random))
+                randomn.append((con, relu, cla, rel))
+    write_data("rand-hyper", hyper + subsample(randomn))
     write_data("hyper", hyper) 
     write_data("shuffle-hyper", hyper + shuffle(hyper)) 
 
